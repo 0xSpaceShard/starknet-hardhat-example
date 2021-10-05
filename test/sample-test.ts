0885908f0f5ca3aacb234355be0cc8fdd255341f
FabijanC <fabijan.corak@gmail.com>
2021-10-05 14:16:48 +0200

    Import starknet types in tests

--- test/sample-test.ts
+++ test/sample-test.ts
@@ -1,13 +1,14 @@
 import { expect } from "chai";
 import { starknet } from "hardhat";
+import { StarknetContract, StarknetContractFactory } from "hardhat/types/runtime";
 
 describe("Starknet", function () {
   this.timeout(300_000); // 5 min
   let preservedAddress: string;
 
   it("should work for a fresh deployment", async function () {
-    const contractFactory = await starknet.getContractFactory("contract");
-    const contract = await contractFactory.deploy();
+    const contractFactory: StarknetContractFactory = await starknet.getContractFactory("contract");
+    const contract: StarknetContract = await contractFactory.deploy();
     console.log("Deployed at", contract.address);
     preservedAddress = contract.address;
     await contract.invoke("increase_balance", [10, 20]);
@@ -19,8 +20,8 @@ describe("Starknet", function () {
   });
 
   it("should work for a previously deployed contract", async function() {
-    const contractFactory = await starknet.getContractFactory("contract");
-    const contract = contractFactory.getContractAt(preservedAddress);
+    const contractFactory: StarknetContractFactory = await starknet.getContractFactory("contract");
+    const contract: StarknetContract = contractFactory.getContractAt(preservedAddress);
     const balanceStr = await contract.call("get_balance");
     const balance = parseInt(balanceStr);
     expect(balance).to.equal(30);
