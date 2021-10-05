77262dd97dd494cefeedc770bdf62107d1161705
FabijanC <fabijan.corak@gmail.com>
2021-10-05 13:25:57 +0200

    Reflect factory introduction in tests

--- test/sample-test.ts
+++ test/sample-test.ts
@@ -1,28 +1,26 @@
 import { expect } from "chai";
-import { getStarknetContract } from "@shardlabs/starknet-hardhat-plugin";
+import { starknet } from "hardhat";
 
 describe("Starknet", function () {
   this.timeout(300_000); // 5 min
   let preservedAddress: string;
 
-  it("Should work", async function () {
-    const contract = await getStarknetContract("contract");
-    await contract.deploy();
+  it("should work for a fresh deployment", async function () {
+    const contractFactory = await starknet.getContractFactory("contract");
+    const contract = await contractFactory.deploy();
     console.log("Deployed at", contract.address);
     preservedAddress = contract.address;
     await contract.invoke("increase_balance", [10, 20]);
     console.log("Increased by 10+20");
-    // await contract.invoke("increase_balance", [20]);
-    // console.log("Increased by 20");
 
     const balanceStr = await contract.call("get_balance");
     const balance = parseInt(balanceStr);
     expect(balance).to.equal(30);
   });
 
-  it("Should work for a previously deployed contract", async function() {
-    const contract = await getStarknetContract("contract");
-    contract.address = preservedAddress;
+  it("should work for a previously deployed contract", async function() {
+    const contractFactory = await starknet.getContractFactory("contract");
+    const contract = contractFactory.getContractAt(preservedAddress);
     const balanceStr = await contract.call("get_balance");
     const balance = parseInt(balanceStr);
     expect(balance).to.equal(30);
