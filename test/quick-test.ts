e10b0c1f46a949afad2cfce21770446a15948fac
Diogo Ribeiro <dr@shardlabs.io>
2022-01-12 14:03:06 +0000

    Added test for contract salt

--- test/quick-test.ts
+++ test/quick-test.ts
@@ -3,9 +3,11 @@ import { starknet } from "hardhat";
 import { StarknetContract, StarknetContractFactory } from "hardhat/types/runtime";
 import { TIMEOUT } from "./constants";
 
+const EXPECTED_ADDRESS = "0x0479ace715103887f28e331401eb05bfe8c4bf3c3efa6943367c9e741f23297c";
+
 describe("Starknet", function() {
     this.timeout(TIMEOUT);
-    it("should work for a fresh deployment", async function() {
+    /*it("should work for a fresh deployment", async function() {
         console.log("Started deployment");
         const contractFactory: StarknetContractFactory = await starknet.getContractFactory("contract");
         const contract: StarknetContract = await contractFactory.deploy({ initial_balance: 0 });
@@ -19,5 +21,17 @@ describe("Starknet", function() {
     
         const { res: balanceAfter } = await contract.call("get_balance");
         expect(balanceAfter).to.deep.equal(30n);
+    });
+*/
+    it("deploy of a contract with salt should provide an expected address", async function() {
+        console.log("Started deployment");
+        const addressSalt: string = "0x99";
+        const contractFactory: StarknetContractFactory = await starknet.getContractFactory("contract");
+
+        const contract: StarknetContract = await contractFactory.deploy({ initial_balance: 0 },addressSalt);
+        console.log("Deployed at", contract.address);
+
+        expect(contract.address).to.deep.equal(EXPECTED_ADDRESS);
+
     });
 });
