4aac558ecbccb3ec367ce1c12d323414851b6f2a
Antun Badurina <badurina.antun@gmail.com>
2022-02-15 12:03:24 +0100

    Add load method test

--- test/postman.test.ts
+++ test/postman.test.ts
@@ -1,18 +1,62 @@
-import { expect, assert } from 'chai';
-import { starknet, network } from 'hardhat';
-import { StarknetContract, StarknetContractFactory } from 'hardhat/types';
-
-const ERROR_MESSAGE =
-  'Request failed. Check if your network has postman endpoint';
+import { assert } from 'chai'
+import { starknet} from 'hardhat'
+import {
+  StarknetContractFactory,
+  StarknetContract,
+} from 'hardhat/types'
 
 describe.only('Postman', () => {
-  it('flush should be defined', () => {
-    expect(starknet.devnet.flush).not.to.be.undefined;
-  });
+  let L2contractFactory: StarknetContractFactory
+  let L2contract: StarknetContract
+  let networkUrl: string
 
-  it('flush pass', async () => {
-    assert.doesNotThrow(async () => {
-      await starknet.devnet.flush();
+  let user = 1
+
+  before(async function () {
+    L2contractFactory = await starknet.getContractFactory('l1l2')
+    L2contract = await L2contractFactory.deploy()
+
+    networkUrl = 'http://127.0.0.1:8545'
+  })
+
+  it('should load the same deployed contract', async () => {
+    const {
+      address: deployedTo,
+      l1_provider: L1Provider
+    } = (await starknet.devnet.loadL1MessagingContract(networkUrl)) as any
+
+    const {
+      address: loadedFrom,
+    } = (await starknet.devnet.loadL1MessagingContract(networkUrl, deployedTo)) as any
+
+
+    assert.equal(L1Provider, networkUrl)
+    assert.equal(deployedTo, loadedFrom)
+  })
+
+  it.skip('should work', async () => {
+    // call again
+    const { address } = (await starknet.devnet.loadL1MessagingContract(
+      networkUrl,
+    )) as any
+
+    console.log(await L2contract.call('get_balance', { user }))
+
+    await L2contract.invoke('increase_balance', {
+      user,
+      amount: 100,
     })
-  });
-});
+
+    console.log(await L2contract.call('get_balance', { user }))
+
+    await L2contract.invoke('withdraw', {
+      user,
+      amount: 10,
+      L1_CONTRACT_ADDRESS: BigInt(address), // this should be
+    })
+
+    console.log(await L2contract.call('get_balance', { user }))
+
+    console.log(JSON.stringify(await starknet.devnet.flush()))
+  })
+})
