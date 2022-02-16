39b8d8dae7c5cb9041b08399c9be910ac99748ae
Antun Badurina <badurina.antun@gmail.com>
2022-02-16 19:19:37 +0100

    Revert default venv path

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,7 +10,7 @@ const config: HardhatUserConfig = {
   starknet: {
     dockerizedVersion: "0.7.1", // alternatively choose one of the two venv options below
     // uses (my-venv) defined by `python -m venv path/to/my-venv`
-    // venv: "env",
+    // venv: "path/to/my-venv",
 
     // uses the currently active Python environment (hopefully with available Starknet commands!)
     // venv: "active"
