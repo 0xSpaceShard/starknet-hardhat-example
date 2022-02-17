3806df5b89b279346f2bf1a066a76a0c3de5a84e
FabijanC <fabijan.corak@gmail.com>
2022-02-17 16:15:57 +0100

    Increment dockerizedVersion in config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,7 @@ import "@shardlabs/starknet-hardhat-plugin";
  */
 const config: HardhatUserConfig = {
   starknet: {
-    dockerizedVersion: "0.7.0", // alternatively choose one of the two venv options below
+    dockerizedVersion: "0.7.1", // alternatively choose one of the two venv options below
     // uses (my-venv) defined by `python -m venv path/to/my-venv`
     // venv: "path/to/my-venv"
     
