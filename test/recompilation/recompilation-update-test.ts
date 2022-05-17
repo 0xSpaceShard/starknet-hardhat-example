22b99a0061a57c422f28ac466a996437fded394c
Nathan-SL <nl@shardlabs.io>
2022-05-17 10:42:10 +0300

    Remove backtick & formatting

--- test/recompilation/recompilation-update-test.ts
+++ test/recompilation/recompilation-update-test.ts
@@ -12,8 +12,8 @@ describe("Starknet", function () {
     it("should check updated contract with new function after recompilation", async function () {
         const contract: StarknetContract = await helloContractFactory.deploy();
         console.log("Deployed at", contract.address);
-    
+
         const { res: balanceBefore } = await contract.call("get_balance");
         expect(balanceBefore).to.deep.equal(0n);
     });
-});``
+});
