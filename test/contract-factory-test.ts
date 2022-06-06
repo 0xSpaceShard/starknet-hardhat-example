33e390420e3224e09d4d7e93a16444218f618d10
edwin <edwinswatpako@gmail.com>
2022-06-06 13:03:23 +0300

    Test loading artifacts from outside artifacts folder

--- test/contract-factory-test.ts
+++ test/contract-factory-test.ts
@@ -4,4 +4,8 @@ describe("ContractFactory", () => {
     it("should be created", async () => {
         await starknet.getContractFactory("contract");
     });
+
+    it('load artifact outside artifacts folder', async () => {
+        await starknet.getContractFactory("../test/test-artifacts/contract");
+    })
 });
