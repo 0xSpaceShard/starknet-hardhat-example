4d187a9e8d01a778aef76b8c8dc1ec4c0ed262ca
edwin <edwinswatpako@gmail.com>
2022-06-14 15:16:32 +0300

    Reverted changes in quick-test

--- test/quick-test.ts
+++ test/quick-test.ts
@@ -3,30 +3,22 @@ import { starknet } from "hardhat";
 import { StarknetContract, StarknetContractFactory } from "hardhat/types/runtime";
 import { TIMEOUT } from "./constants";
 
-async function quickTest(artifactsPathSegment: string) {
-    console.log("Started deployment");
-    const contractFactory: StarknetContractFactory = await starknet.getContractFactory(artifactsPathSegment);
-    const contract: StarknetContract = await contractFactory.deploy({ initial_balance: 0 });
-    console.log("Deployed at", contract.address);
-
-    const { res: balanceBefore } = await contract.call("get_balance");
-    expect(balanceBefore).to.deep.equal(0n);
-
-    await contract.invoke("increase_balance", { amount1: 10, amount2: 20 });
-    console.log("Increased by 10 + 20");
-
-    const { res: balanceAfter } = await contract.call("get_balance");
-    expect(balanceAfter).to.deep.equal(30n);
-}
-
 describe("Starknet", function () {
     this.timeout(TIMEOUT);
 
     it("should work for a fresh deployment", async function () {
-        await quickTest("contract");
-    });
+        console.log("Started deployment");
+        const contractFactory: StarknetContractFactory = await starknet.getContractFactory("contract");
+        const contract: StarknetContract = await contractFactory.deploy({ initial_balance: 0 });
+        console.log("Deployed at", contract.address);
 
-    it("quick test for relative artifacts", async function () {
-        await quickTest("../test/test-aritfacts/contract")
+        const { res: balanceBefore } = await contract.call("get_balance");
+        expect(balanceBefore).to.deep.equal(0n);
+
+        await contract.invoke("increase_balance", { amount1: 10, amount2: 20 });
+        console.log("Increased by 10 + 20");
+
+        const { res: balanceAfter } = await contract.call("get_balance");
+        expect(balanceAfter).to.deep.equal(30n);
     });
 });
