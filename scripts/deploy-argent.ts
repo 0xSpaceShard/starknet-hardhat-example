995641a77e449ff4bc14ccf10821e54b4ac17fb1
FabijanC <fabijan.corak@gmail.com>
2022-06-21 09:45:54 +0200

    Refactor argent deployment script

--- scripts/deploy-argent.ts
+++ scripts/deploy-argent.ts
@@ -1,35 +1,36 @@
 import hardhat from "hardhat";
-import { ensureEnvVar } from "../test/util";
+import { ArgentAccount } from "hardhat/types/runtime";
+import { ensureEnvVar, expectAddressEquality } from "../test/util";
 
-async function main() {
-    const account = await hardhat.starknet.deployAccount("Argent", {
-      salt: "0x42",
-      privateKey: ensureEnvVar("ARGENT_ACCOUNT_PRIVATE_KEY")
-    });
-    
+describe("Argent account", function () {
+  it("should deploy and initialize", async function () {
+    const account = <ArgentAccount>await hardhat.starknet.deployAccount(
+      "Argent",
+      {
+        salt: "0x42",
+        privateKey: ensureEnvVar("ARGENT_ACCOUNT_PRIVATE_KEY"),
+      }
+    );
+
     const expectedAddress = ensureEnvVar("ARGENT_ACCOUNT_ADDRESS");
-    if (BigInt(account.address) !== BigInt(expectedAddress)) {
-      console.error(`Error! Actual: ${account.address}, expected: ${expectedAddress}`)
-      process.exit(1);
-    }
+    expectAddressEquality(account.address, expectedAddress);
 
-    // initializing the Argent account requires invoke `initialize` with a funded account
+    // initializing Argent account requires invoking `initialize` with a funded account
     const fundedAccount = await hardhat.starknet.getAccountFromAddress(
       ensureEnvVar("OZ_ACCOUNT_ADDRESS"),
       ensureEnvVar("OZ_ACCOUNT_PRIVATE_KEY"),
       "OpenZeppelin"
     );
-    await fundedAccount.invoke(account.starknetContract, "initialize", {
-      signer: account.publicKey,
-      guardian: "0"
-    }, {
-      maxFee: 1e18
-    });
-}
-
-main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+    await fundedAccount.invoke(
+      account.starknetContract,
+      "initialize",
+      {
+        signer: account.publicKey,
+        guardian: account.guardianPublicKey || "0",
+      },
+      {
+        maxFee: 1e18,
+      }
+    );
+  });
+});
--- test/util.ts
+++ test/util.ts
@@ -13,3 +13,24 @@ export function ensureEnvVar(varName: string): string {
     }
     return <string>process.env[varName];
 }
+
+
+/**
+ * Receives a hex address, converts it to bigint, converts it back to hex.
+ * This is done to strip leading zeros.
+ * @param address a hex string representation of an address
+ * @returns an adapted hex string representation of the address
+ */
+ function adaptAddress(address: string) {
+    return "0x" + BigInt(address).toString(16);
+  }
+  
+  /**
+   * Expects address equality after adapting them.
+   * @param actual 
+   * @param expected 
+   */
+  export function expectAddressEquality(actual: string, expected: string) {
+    expect(adaptAddress(actual)).to.equal(adaptAddress(expected));
+  }
+  
