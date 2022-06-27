b3a1d09a895d28b38282f7044860dc6f8ff6348d
FabijanC <fabijan.corak@gmail.com>
2022-06-27 17:33:07 +0200

    Modify integratedDevnet config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -32,7 +32,7 @@ const config: HardhatUserConfig = {
     integratedDevnet: {
       url: "http://127.0.0.1:5050",
       // venv: "active",
-      dockerizedVersion: "0.2.2",
+      // dockerizedVersion: "<DEVNET_VERSION>",
     },
     hardhat: {},
   },
