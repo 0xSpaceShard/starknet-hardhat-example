7f82c5671f47c7de1d6eee2ebea55ac397c369b0
FabijanC <fabijan.corak@gmail.com>
2022-07-08 16:03:29 +0200

    Add library call example

---
+++ contracts/contract_proxy.cairo
@@ -0,0 +1,63 @@
+%lang starknet
+%builtins pedersen range_check bitwise
+
+from starkware.cairo.common.cairo_builtins import HashBuiltin
+from starkware.starknet.common.syscalls import library_call
+
+@contract_interface
+namespace IBalanceContract:
+    func increase_balance(amount1 : felt, amount2 : felt):
+    end
+
+    func get_balance() -> (res : felt):
+    end
+end
+
+@external
+func call_increase_balance{syscall_ptr : felt*, range_check_ptr}(
+    contract_address : felt, amount1 : felt, amount2 : felt
+):
+    IBalanceContract.increase_balance(
+        contract_address=contract_address, amount1=amount1, amount2=amount2
+    )
+    return ()
+end
+
+@view
+func call_get_balance{syscall_ptr : felt*, range_check_ptr}(
+    contract_address : felt
+) -> (res : felt):
+    let (res) = IBalanceContract.get_balance(
+        contract_address=contract_address
+    )
+    return (res=res)
+end
+
+# Define local balance variable in our proxy contract.
+@storage_var
+func balance() -> (res : felt):
+end
+
+@external
+func increase_my_balance{syscall_ptr : felt*, range_check_ptr}(
+    class_hash : felt, amount1 : felt, amount2 : felt
+):
+    # Increase the local balance variable using a function from a
+    # different contract class using a library call.
+    IBalanceContract.library_call_increase_balance(
+        class_hash=class_hash, amount1=amount1, amount2=amount2
+    )
+    return ()
+end
+
+@view
+func get_my_balance{syscall_ptr : felt*, range_check_ptr}(
+    class_hash : felt
+) -> (res : felt):
+    # Increase the local balance variable using a function from a
+    # different contract class using a library call.
+    let (res) = IBalanceContract.library_call_get_balance(
+        class_hash=class_hash
+    )
+    return (res=res)
+end
---
+++ scripts/library-call.ts
@@ -0,0 +1,65 @@
+import { expect } from "chai";
+import { starknet } from "hardhat";
+import { TIMEOUT } from "../test/constants";
+
+describe("Library call", function () {
+  this.timeout(TIMEOUT);
+
+  it("should modify library contract", async function () {
+    const implementationFactory = await starknet.getContractFactory("contract");
+    const implementation = await implementationFactory.deploy({
+      initial_balance: 0,
+    });
+
+    const proxyFactory = await starknet.getContractFactory("contract_proxy");
+    const proxy = await proxyFactory.deploy();
+    console.log("Deployed proxy to", proxy.address);
+
+    const { res: initialProxyBalance } = await proxy.call("call_get_balance", {
+      contract_address: implementation.address,
+    });
+    expect(initialProxyBalance).to.equal(0n); // proxy is using its own storage
+
+    await proxy.invoke("call_increase_balance", {
+      contract_address: implementation.address,
+      amount1: 10,
+      amount2: 20,
+    });
+
+    const { res: finalProxyBalance } = await proxy.call("call_get_balance", {
+      contract_address: implementation.address,
+    });
+    expect(finalProxyBalance).to.equal(30n);
+
+    const { res: finalImplementationBalance } = await implementation.call(
+      "get_balance"
+    );
+    expect(finalImplementationBalance).to.equal(30n);
+  });
+
+  it("should modify calling contract", async function () {
+    const implementationFactory = await starknet.getContractFactory("contract");
+    const implementationClassHash = await implementationFactory.declare();
+
+    // uses delegate proxy defined in contracts/delegate_proxy.cairo
+    const proxyFactory = await starknet.getContractFactory("contract_proxy");
+    const proxy = await proxyFactory.deploy();
+    console.log("Deployed proxy to", proxy.address);
+
+    const { res: initialProxyBalance } = await proxy.call("get_my_balance", {
+      class_hash: implementationClassHash,
+    });
+    expect(initialProxyBalance).to.equal(0n); // proxy is using its own storage
+
+    await proxy.invoke("increase_my_balance", {
+      class_hash: implementationClassHash,
+      amount1: 10,
+      amount2: 20,
+    });
+
+    const { res: finalProxyBalance } = await proxy.call("get_my_balance", {
+      class_hash: implementationClassHash,
+    });
+    expect(finalProxyBalance).to.equal(30n);
+  });
+});
