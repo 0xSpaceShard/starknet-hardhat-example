43fc7ebe7ab8ce07d709b9fafe398781c59b11bf
FabijanC <fabijan.corak@gmail.com>
2022-07-11 17:49:55 +0200

    Specify integrated-devnet in hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,7 +15,7 @@ const config: HardhatUserConfig = {
 
     // uses the currently active Python environment (hopefully with available Starknet commands!)
     // venv: "active",
-    network: "alpha",
+    network: "integrated-devnet",
     wallets: {
       OpenZeppelin: {
         accountName: "OpenZeppelin",
