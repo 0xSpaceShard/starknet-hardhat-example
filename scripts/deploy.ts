e67bbe0b60ca1fc636927337734d65281ab488f4
Nathan-SL <nl@shardlabs.io>
2022-07-15 09:43:34 +0300

    Add test for fix on invalid signature

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,9 +1,27 @@
 import hardhat from "hardhat";
+import { ensureEnvVar } from "../test/util";
 
 async function main() {
+
+    const account = await starknet.getAccountFromAddress(
+        ensureEnvVar("OZ_ACCOUNT_ADDRESS"),
+        ensureEnvVar("OZ_ACCOUNT_PRIVATE_KEY"),
+        "OpenZeppelin"
+    );
+
     const contractFactory = await hardhat.starknet.getContractFactory("contract");
     const contract = await contractFactory.deploy({ initial_balance: 0 });
+
     console.log("Deployed to:", contract.address);
+    const fee = await account.estimateFee(contract, "increase_balance", { amount1: 10, amount2: 20 });
+    const { res: balanceBefore } = await account.call(contract, "get_balance");
+    console.log("Balance before invoke: ", balanceBefore);
+
+    await account.invoke(contract, "increase_balance", { amount1: 10, amount2: 20 }, {
+        maxFee: fee.amount * 2n,
+    });
+    const { res: balanceAfter } = await account.call(contract, "get_balance");
+    console.log("Balance after invoke:", balanceAfter);
 }
 
 main()
