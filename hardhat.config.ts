eebd68659ff7257babd95ea295e36117395f2783
FabijanC <fabijan.corak@gmail.com>
2022-07-20 12:49:09 +0200

    Specify 0.9.1 in hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,7 +9,7 @@ require("dotenv").config();
 const config: HardhatUserConfig = {
   solidity: "0.6.12",
   starknet: {
-    dockerizedVersion: "0.9.0", // alternatively choose one of the two venv options below
+    dockerizedVersion: "0.9.1", // alternatively choose one of the two venv options below
     // uses (my-venv) defined by `python -m venv path/to/my-venv`
     // venv: "path/to/my-venv",
 
