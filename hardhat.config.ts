d8fa8ca8162b4e791043f21731bc71f5a0b9f6e3
Nathan <102951164+Nathan-SL@users.noreply.github.com>
2022-08-19 15:21:09 +0300

    Eslint, prettier and GitHub action (#74)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,42 +1,42 @@
 import { HardhatUserConfig } from "hardhat/types";
 import "@shardlabs/starknet-hardhat-plugin";
 import "@nomiclabs/hardhat-ethers";
-require("dotenv").config();
+import * as dotenv from "dotenv";
+dotenv.config();
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 const config: HardhatUserConfig = {
-  solidity: "0.6.12",
-  starknet: {
-    dockerizedVersion: "0.9.1", // alternatively choose one of the two venv options below
-    // uses (my-venv) defined by `python -m venv path/to/my-venv`
-    // venv: "path/to/my-venv",
+    solidity: "0.6.12",
+    starknet: {
+        dockerizedVersion: "0.9.1", // alternatively choose one of the two venv options below
+        // uses (my-venv) defined by `python -m venv path/to/my-venv`
+        // venv: "path/to/my-venv",
 
-    // uses the currently active Python environment (hopefully with available Starknet commands!)
-    // venv: "active",
-    recompile: false,
-    network: "integrated-devnet",
-    wallets: {
-      OpenZeppelin: {
-        accountName: "OpenZeppelin",
-        modulePath:
-          "starkware.starknet.wallets.open_zeppelin.OpenZeppelinAccount",
-        accountPath: "~/.starknet_accounts",
-      },
+        // uses the currently active Python environment (hopefully with available Starknet commands!)
+        // venv: "active",
+        recompile: false,
+        network: "integrated-devnet",
+        wallets: {
+            OpenZeppelin: {
+                accountName: "OpenZeppelin",
+                modulePath: "starkware.starknet.wallets.open_zeppelin.OpenZeppelinAccount",
+                accountPath: "~/.starknet_accounts"
+            }
+        }
     },
-  },
-  networks: {
-    devnet: {
-      url: "http://127.0.0.1:5050",
-    },
-    integratedDevnet: {
-      url: "http://127.0.0.1:5050",
-      // venv: "active",
-      // dockerizedVersion: "<DEVNET_VERSION>",
-    },
-    hardhat: {},
-  },
+    networks: {
+        devnet: {
+            url: "http://127.0.0.1:5050"
+        },
+        integratedDevnet: {
+            url: "http://127.0.0.1:5050"
+            // venv: "active",
+            // dockerizedVersion: "<DEVNET_VERSION>",
+        },
+        hardhat: {}
+    }
 };
 
 export default config;
--- scripts/library-call.ts
+++ scripts/library-call.ts
@@ -3,68 +3,66 @@ import { starknet } from "hardhat";
 import { TIMEOUT } from "../test/constants";
 
 describe("Library call", function () {
-  this.timeout(TIMEOUT);
+    this.timeout(TIMEOUT);
 
-  it("should modify implementation contract", async function () {
-    const implementationFactory = await starknet.getContractFactory("contract");
-    const implementation = await implementationFactory.deploy({
-      initial_balance: 0,
+    it("should modify implementation contract", async function () {
+        const implementationFactory = await starknet.getContractFactory("contract");
+        const implementation = await implementationFactory.deploy({
+            initial_balance: 0
+        });
+
+        await implementation.invoke("increase_balance", {
+            amount1: 10,
+            amount2: 20
+        });
+
+        const proxyFactory = await starknet.getContractFactory("contract_proxy");
+        const proxy = await proxyFactory.deploy();
+        console.log("Deployed proxy to", proxy.address);
+
+        const { res: initialProxyBalance } = await proxy.call("call_get_balance", {
+            contract_address: implementation.address
+        });
+        expect(initialProxyBalance).to.equal(30n); // proxy NOT using its own storage
+
+        await proxy.invoke("call_increase_balance", {
+            contract_address: implementation.address,
+            amount1: 10,
+            amount2: 20
+        });
+
+        const { res: finalProxyBalance } = await proxy.call("call_get_balance", {
+            contract_address: implementation.address
+        });
+        expect(finalProxyBalance).to.equal(60n);
+
+        const { res: finalImplementationBalance } = await implementation.call("get_balance");
+        expect(finalImplementationBalance).to.equal(60n); // proxy and implementation used same storage
     });
 
-    await implementation.invoke("increase_balance", {
-      amount1: 10,
-      amount2: 20,
+    it("should modify calling contract", async function () {
+        const implementationFactory = await starknet.getContractFactory("contract");
+        const implementationClassHash = await implementationFactory.declare();
+
+        // uses delegate proxy defined in contracts/delegate_proxy.cairo
+        const proxyFactory = await starknet.getContractFactory("contract_proxy");
+        const proxy = await proxyFactory.deploy();
+        console.log("Deployed proxy to", proxy.address);
+
+        const { res: initialProxyBalance } = await proxy.call("get_my_balance", {
+            class_hash: implementationClassHash
+        });
+        expect(initialProxyBalance).to.equal(0n); // proxy is using its own storage
+
+        await proxy.invoke("increase_my_balance", {
+            class_hash: implementationClassHash,
+            amount1: 10,
+            amount2: 20
+        });
+
+        const { res: finalProxyBalance } = await proxy.call("get_my_balance", {
+            class_hash: implementationClassHash
+        });
+        expect(finalProxyBalance).to.equal(30n);
     });
-
-    const proxyFactory = await starknet.getContractFactory("contract_proxy");
-    const proxy = await proxyFactory.deploy();
-    console.log("Deployed proxy to", proxy.address);
-
-    const { res: initialProxyBalance } = await proxy.call("call_get_balance", {
-      contract_address: implementation.address,
-    });
-    expect(initialProxyBalance).to.equal(30n); // proxy NOT using its own storage
-
-    await proxy.invoke("call_increase_balance", {
-      contract_address: implementation.address,
-      amount1: 10,
-      amount2: 20,
-    });
-
-    const { res: finalProxyBalance } = await proxy.call("call_get_balance", {
-      contract_address: implementation.address,
-    });
-    expect(finalProxyBalance).to.equal(60n);
-
-    const { res: finalImplementationBalance } = await implementation.call(
-      "get_balance"
-    );
-    expect(finalImplementationBalance).to.equal(60n); // proxy and implementation used same storage
-  });
-
-  it("should modify calling contract", async function () {
-    const implementationFactory = await starknet.getContractFactory("contract");
-    const implementationClassHash = await implementationFactory.declare();
-
-    // uses delegate proxy defined in contracts/delegate_proxy.cairo
-    const proxyFactory = await starknet.getContractFactory("contract_proxy");
-    const proxy = await proxyFactory.deploy();
-    console.log("Deployed proxy to", proxy.address);
-
-    const { res: initialProxyBalance } = await proxy.call("get_my_balance", {
-      class_hash: implementationClassHash,
-    });
-    expect(initialProxyBalance).to.equal(0n); // proxy is using its own storage
-
-    await proxy.invoke("increase_my_balance", {
-      class_hash: implementationClassHash,
-      amount1: 10,
-      amount2: 20,
-    });
-
-    const { res: finalProxyBalance } = await proxy.call("get_my_balance", {
-      class_hash: implementationClassHash,
-    });
-    expect(finalProxyBalance).to.equal(30n);
-  });
 });
