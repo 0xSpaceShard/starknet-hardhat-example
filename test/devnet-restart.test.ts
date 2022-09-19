16c73f7667c9d457b98157f8a40f6236cd485c3b
FabijanC <fabijan.corak@gmail.com>
2022-09-19 17:34:10 +0200

    Invoke through account

--- test/devnet-restart.test.ts
+++ test/devnet-restart.test.ts
@@ -1,10 +1,22 @@
 import { expect } from "chai";
 import { starknet } from "hardhat";
+import { Account } from "hardhat/types";
 
 import { TIMEOUT } from "./constants";
+import { ensureEnvVar } from "./util";
 
 describe("Devnet restart", function () {
     this.timeout(TIMEOUT);
+
+    let account: Account;
+
+    before(async function () {
+        account = await starknet.getAccountFromAddress(
+            ensureEnvVar("OZ_ACCOUNT_ADDRESS"),
+            ensureEnvVar("OZ_ACCOUNT_PRIVATE_KEY"),
+            "OpenZeppelin"
+        );
+    });
 
     it("should pass", async () => {
         const response = await starknet.devnet.restart();
@@ -15,14 +27,14 @@ describe("Devnet restart", function () {
         const contractFactory = await starknet.getContractFactory("contract");
         const contract = await contractFactory.deploy({ initial_balance: 0 });
 
-        await contract.invoke("increase_balance", { amount1: 10, amount2: 20 });
+        await account.invoke(contract, "increase_balance", { amount1: 10, amount2: 20 });
         const { res: balanceAfter } = await contract.call("get_balance");
         expect(balanceAfter).to.deep.equal(30n);
 
         await starknet.devnet.restart();
 
         try {
-            await contract.invoke("increase_balance", { amount1: 10, amount2: 20 });
+            await account.invoke(contract, "increase_balance", { amount1: 10, amount2: 20 });
             expect.fail("Should throw");
         } catch (error: any) {
             expect(error.message).to.contain("No contract at the provided address");
@@ -49,7 +61,7 @@ describe("Devnet restart", function () {
         const contract = await contractFactory.deploy({ initial_balance: 0 }, { salt });
         const initialAddress = contract.address;
 
-        await contract.invoke("increase_balance", { amount1: 10, amount2: 20 });
+        await account.invoke(contract, "increase_balance", { amount1: 10, amount2: 20 });
         const { res: initialBalance } = await contract.call("get_balance");
         expect(initialBalance).to.deep.equal(30n);
 
@@ -60,7 +72,7 @@ describe("Devnet restart", function () {
         const { res: balanceAfterRestart } = await contract.call("get_balance");
         expect(balanceAfterRestart).to.deep.equal(0n);
 
-        await contract.invoke("increase_balance", { amount1: 10, amount2: 20 });
+        await account.invoke(contract, "increase_balance", { amount1: 10, amount2: 20 });
         expect(contract.address).to.equal(initialAddress);
 
         const { res: balanceAfterInvoke } = await contract.call("get_balance");
