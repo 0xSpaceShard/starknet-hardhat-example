991c231c2d98b577ac3516571867c316aa6c1dcc
FabijanC <fabijan.corak@gmail.com>
2022-09-20 10:39:57 +0200

    Fix proxy/delegate scripts

--- scripts/delegate-proxy.ts
+++ scripts/delegate-proxy.ts
@@ -1,13 +1,20 @@
 import { expect } from "chai";
 import { starknet } from "hardhat";
 import { TIMEOUT } from "../test/constants";
+import { ensureEnvVar } from "../test/util";
 
 describe("Delegate proxy", function () {
     this.timeout(TIMEOUT);
 
     it("should forward to the implementation contract", async function () {
+        const account = await starknet.getAccountFromAddress(
+            ensureEnvVar("OZ_ACCOUNT_ADDRESS"),
+            ensureEnvVar("OZ_ACCOUNT_PRIVATE_KEY"),
+            "OpenZeppelin"
+        );
+
         const implementationFactory = await starknet.getContractFactory("contract");
-        const implementationClassHash = await implementationFactory.declare();
+        const implementationClassHash = await account.declare(implementationFactory);
 
         // uses delegate proxy defined in contracts/delegate_proxy.cairo
         const proxyFactory = await starknet.getContractFactory("delegate_proxy");
@@ -21,7 +28,7 @@ describe("Delegate proxy", function () {
         const { res: initialProxyBalance } = await proxy.call("get_balance");
         expect(initialProxyBalance).to.equal(0n); // proxy is using its own storage
 
-        await proxy.invoke("increase_balance", { amount1: 10, amount2: 20 });
+        await account.invoke(proxy, "increase_balance", { amount1: 10, amount2: 20 });
         const { res: finalProxyBalance } = await proxy.call("get_balance");
         expect(finalProxyBalance).to.equal(30n);
     });
