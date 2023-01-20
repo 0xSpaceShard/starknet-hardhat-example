06cefdd7c59f78dfad8a1c5359b4f50d7bf75772
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-01-20 18:27:37 +0800

    #89 Test updates

--- test/devnet-mint.test.ts
+++ test/devnet-mint.test.ts
@@ -6,9 +6,9 @@ import { TIMEOUT } from "./constants";
 describe("Devnet mint", function () {
     this.timeout(TIMEOUT);
     let balance = 0;
-    const address = "0x22edb233f3bfb99d2e9a14d283794515d124ef94398e143906d379348693fb7";
-    const div = 1e16; // To round off large numbers
-    const diff = 25 * div;
+    // Randomly chosen address
+    const address = "0x22ed43906d3793bfb99db233f3e9a14d283794515d124ef94398e1248693fb7";
+    const diff = 25e4;
 
     it("should have valid response", async () => {
         const resp = await starknet.devnet.mint(address, 0, true);
@@ -21,23 +21,26 @@ describe("Devnet mint", function () {
     });
 
     it("should tx_hash in separate transaction (not lite) mode", async () => {
-        const resp = await starknet.devnet.mint(address, 0, true);
+        const resp = await starknet.devnet.mint(address, 0, false);
+
         const { new_balance, unit, tx_hash } = resp;
         balance = new_balance;
 
         expect(typeof new_balance).to.equal("number");
         expect(unit).to.equal("wei");
-        expect(tx_hash).to.equal(null);
+        expect(typeof tx_hash).to.equal("string");
+        expect(tx_hash.indexOf("0x")).to.equal(0);
     });
 
     it("should mint lite mode", async () => {
         const resp = await starknet.devnet.mint(address, diff, true);
-        expect(Math.round(resp.new_balance / div - balance / div)).to.deep.equal(diff / div);
+        expect(Math.round(resp.new_balance)).to.deep.equal(diff + balance);
         balance = resp.new_balance; // Update balance for next test
     });
+
     it("should mint in separate transaction (not lite)", async () => {
         const resp = await starknet.devnet.mint(address, diff, false);
-        expect(Math.round(resp.new_balance / div - balance / div)).to.deep.equal(diff / div);
+        expect(Math.round(resp.new_balance)).to.deep.equal(diff + balance);
         balance = resp.new_balance; // Update balance for next test
     });
 });
