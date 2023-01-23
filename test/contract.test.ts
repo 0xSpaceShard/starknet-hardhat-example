fc6febe0fca5af309fa4112ce12f5ec8de45f2da
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-01-23 14:27:52 +0800

    Create contract.test.ts
    Shard-Labs/starknet-hardhat-plugin#273

---
+++ test/contract.test.ts
@@ -0,0 +1,66 @@
+import { expect } from "chai";
+import { starknet } from "hardhat";
+import { TIMEOUT } from "./constants";
+import { StarknetContractFactory } from "hardhat/types/runtime";
+import { getPredeployedOZAccount } from "./util";
+import { OpenZeppelinAccount } from "@shardlabs/starknet-hardhat-plugin/dist/src/account";
+import * as fs from "fs";
+import { StarknetContract } from "@shardlabs/starknet-hardhat-plugin/dist/src/types";
+
+describe("StarknetContractFactory", function () {
+    this.timeout(TIMEOUT);
+    let contractFactory: StarknetContractFactory;
+    let account: OpenZeppelinAccount;
+    let contract: StarknetContract;
+    let initial_balance = 25n;
+
+    /**
+ * invoke(functionName: string, args?: StringMap, options?: InvokeOptions): Promise<InvokeResponse>;
+ * call(functionName: string, args?: StringMap, options?: CallOptions): Promise<StringMap>;
+ * estimateFee(functionName: string, args?: StringMap, options?: EstimateFeeOptions): Promise<starknet.FeeEstimation>;
+ * getAbi(): starknet.Abi;
+  decodeEvents(events: starknet.Event[]): DecodedEvent[];
+ */
+
+    before(async function () {
+        account = await getPredeployedOZAccount();
+        contractFactory = await starknet.getContractFactory("contract");
+        contract = await account.deploy(contractFactory, { initial_balance });
+    });
+    it("should have address", async () => {
+        const { address } = account;
+        expect(typeof address).to.be.eq("string");
+        expect(address.indexOf("0x")).to.be.eq(0);
+    });
+    it("should have valid abi getAbi", async () => {
+        const abi = contract.getAbi();
+        expect(typeof abi).to.be.eq("object");
+        expect(typeof abi.increase_balance).to.be.eq("object");
+        expect(abi.increase_balance.type).to.be.eq("function");
+    });
+    it("should call", async () => {
+        const resp = await contract.call("get_balance");
+        console.log(resp);
+        expect(resp.res).to.be.eq(initial_balance);
+    });
+    it("should invoke", async () => {
+        const txnHash = await account.invoke(contract, "increase_balance", {
+            amount1: 20,
+            amount2: 5
+        });
+        expect(typeof txnHash).to.be.eq("string");
+        expect(txnHash.indexOf("0x")).to.be.eq(0);
+    });
+    it("should estimateFee", async () => {
+        const feeEstimation = await account.estimateFee(contract, "increase_balance", {
+            amount1: 20,
+            amount2: 5
+        });
+        expect(typeof feeEstimation.amount).to.be.eq("bigint");
+        expect(typeof feeEstimation.unit).to.be.eq("string");
+        expect(typeof feeEstimation.gas_price).to.be.eq("bigint");
+        expect(typeof feeEstimation.gas_usage).to.be.eq("bigint");
+    });
+
+    // Decode events test in test/decode-events.test.ts
+});
