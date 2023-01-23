d366cc2100cc944032d2874c087cadeb00200ae9
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-01-23 14:27:44 +0800

    Factory tests
    Shard-Labs/starknet-hardhat-plugin#273

---
+++ test/contract-factory.test.ts
@@ -0,0 +1,57 @@
+import { expect } from "chai";
+import { starknet } from "hardhat";
+import { TIMEOUT } from "./constants";
+import { StarknetContractFactory } from "hardhat/types/runtime";
+import { getPredeployedOZAccount } from "./util";
+import { OpenZeppelinAccount } from "@shardlabs/starknet-hardhat-plugin/dist/src/account";
+import * as fs from "fs";
+import { StarknetContract } from "@shardlabs/starknet-hardhat-plugin/dist/src/types";
+
+describe("StarknetContractFactory", function () {
+    this.timeout(TIMEOUT);
+    let contractFactory: StarknetContractFactory;
+    let account: OpenZeppelinAccount;
+    // let contract: StarknetContract;
+
+    before(async function () {
+        account = await getPredeployedOZAccount();
+        contractFactory = await starknet.getContractFactory("contract");
+
+        // contract = await account.deploy(contractFactory, { initial_balance: 0 });
+    });
+    it("should have file at getAbiPath()", async () => {
+        const abiPath = contractFactory.getAbiPath();
+        expect(fs.existsSync(abiPath)).to.be.true;
+    });
+    it("should have abi", async () => {
+        const { abi } = contractFactory;
+        expect(typeof abi).to.be.eq("object");
+        expect(abi).to.not.be.null;
+        expect(Object.keys(abi).length > 0).to.be.true;
+    });
+    it("should have increase_balance in abi", async () => {
+        const { increase_balance } = contractFactory.abi;
+        expect(typeof increase_balance).to.be.eq("object");
+        expect(increase_balance.type).to.be.eq("function");
+    });
+    it("should have file at metadataPath", async () => {
+        const { metadataPath } = contractFactory;
+        expect(fs.existsSync(metadataPath)).to.be.true;
+    });
+    it("should declare class", async () => {
+        // account.declare calls and return contractFactory.declare
+        const classHash = await account.declare(contractFactory);
+        expect(typeof classHash).to.be.eq("string");
+        expect(classHash.indexOf("0x")).to.be.eq(0);
+    });
+    it("should getClassHash", async () => {
+        const classHash = await contractFactory.getClassHash();
+        expect(typeof classHash).to.be.eq("string");
+        expect(classHash.indexOf("0x")).to.be.eq(0);
+    });
+    it("should getContractAt", async () => {
+        const { address } = await account.deploy(contractFactory, { initial_balance: 0 });
+        const contract = contractFactory.getContractAt(address);
+        expect(contract instanceof StarknetContract).to.be.true;
+    });
+});
