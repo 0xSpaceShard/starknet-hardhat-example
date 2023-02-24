c7c04f5222dd0c8bce2e94ce76e2645d7e65e66d
FabijanC <fabijan.corak@gmail.com>
2023-02-24 10:12:43 +0100

    Update solidity messaging contracts

--- contracts/IStarknetMessaging.sol
+++ contracts/IStarknetMessaging.sol
@@ -1,54 +1,54 @@
 // SPDX-License-Identifier: Apache-2.0.
 pragma solidity ^0.6.12;
 
-interface IStarknetMessaging {
-    // This event needs to be compatible with the one defined in Output.sol.
-    event LogMessageToL1(
-        uint256 indexed from_address,
-        address indexed to_address,
-        uint256[] payload
-    );
+import "./IStarknetMessagingEvents.sol";
 
-    // An event that is raised when a message is sent from L1 to L2.
-    event LogMessageToL2(
-        address indexed from_address,
-        uint256 indexed to_address,
-        uint256 indexed selector,
-        uint256[] payload,
-        uint256 nonce
-    );
-
-    // An event that is raised when a message from L2 to L1 is consumed.
-    event ConsumedMessageToL1(
-        uint256 indexed from_address,
-        address indexed to_address,
-        uint256[] payload
-    );
-
-    // An event that is raised when a message from L1 to L2 is consumed.
-    event ConsumedMessageToL2(
-        address indexed from_address,
-        uint256 indexed to_address,
-        uint256 indexed selector,
-        uint256[] payload,
-        uint256 nonce
-    );
-
+interface IStarknetMessaging is IStarknetMessagingEvents {
     /**
       Sends a message to an L2 contract.
-      Returns the hash of the message.
+      This function is payable, the payed amount is the message fee.
+
+      Returns the hash of the message and the nonce of the message.
     */
     function sendMessageToL2(
-        uint256 to_address,
+        uint256 toAddress,
         uint256 selector,
         uint256[] calldata payload
-    ) external returns (bytes32);
+    ) external payable returns (bytes32, uint256);
 
     /**
       Consumes a message that was sent from an L2 contract.
+
       Returns the hash of the message.
     */
     function consumeMessageFromL2(uint256 fromAddress, uint256[] calldata payload)
         external
         returns (bytes32);
+
+    /**
+      Starts the cancellation of an L1 to L2 message.
+      A message can be canceled messageCancellationDelay() seconds after this function is called.
+
+      Note: This function may only be called for a message that is currently pending and the caller
+      must be the sender of the that message.
+    */
+    function startL1ToL2MessageCancellation(
+        uint256 toAddress,
+        uint256 selector,
+        uint256[] calldata payload,
+        uint256 nonce
+    ) external returns (bytes32);
+
+    /**
+      Cancels an L1 to L2 message, this function should be called messageCancellationDelay() seconds
+      after the call to startL1ToL2MessageCancellation().
+
+      Note that the message fee is not refunded.
+    */
+    function cancelL1ToL2Message(
+        uint256 toAddress,
+        uint256 selector,
+        uint256[] calldata payload,
+        uint256 nonce
+    ) external returns (bytes32);
 }
