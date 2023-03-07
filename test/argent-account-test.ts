0c6b913dfaada7d5eea023f568fc2aeeaf3aabc9
Thomas S. Bauer <tsbauer@posteo.eu>
2023-03-07 19:32:10 +0100

    Revert OZ to argent account

--- test/argent-account-test.ts
+++ test/argent-account-test.ts
@@ -183,14 +183,14 @@ describe("Argent account", function () {
     });
 
     it("should return hash for rejected declare tx if maxFee insufficient", async function () {
-        const account = await getOZAccount();
+        const account = await getArgentAccount();
         const txHash = await account.declare(mainContractFactory, { maxFee: 1 });
         const txReceipt = await starknet.getTransactionReceipt(txHash);
         expect(txReceipt.status).to.equal("REJECTED");
     });
 
     it("should return hash for accepted declare tx if maxFee sufficient", async function () {
-        const account = await getOZAccount();
+        const account = await getArgentAccount();
         const txHash = await account.declare(mainContractFactory, { maxFee: 1e18 });
         const txReceipt = await starknet.getTransactionReceipt(txHash);
         expect(txReceipt.status).to.equal("ACCEPTED_ON_L2" || "ACCEPTED_ON_L1");
