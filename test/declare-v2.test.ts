514fd9ffc1434b561f6a3f7a4a7f32e03281cb6a
Nathan-SL <nl@shardlabs.io>
2023-04-04 12:16:46 +0300

    Update declare test

--- test/declare-v2.test.ts
+++ test/declare-v2.test.ts
@@ -9,23 +9,30 @@ describe("Class declaration", function () {
         const account = await getOZAccount();
 
         const contractFactory = await starknet.getContractFactory("cairo1");
+        // const declareFee = await account.estimateDeclareFee(contractFactory);
+        // console.log("Estimated declare fee: ", declareFee);
         const declareTxHash = await account.declareV2(contractFactory);
-        console.log("Declare Tx Hash: ", declareTxHash);
-        const deployFee = await account.estimateDeployFee(contractFactory);
+        console.log("Declare v2 Tx Hash: ", declareTxHash);
+        const deployFee = await account.estimateDeployFee(contractFactory, {
+            initial_balance: 10n
+        });
         console.log("Estimated deploy fee: ", deployFee);
 
-        const contract = await account.deploy(contractFactory, {}, { maxFee: 1e18 });
-        const { res: balanceBefore } = await contract.call("get_balance");
-        expect(balanceBefore).to.deep.equal(0n);
+        const contract = await account.deploy(contractFactory, {
+            initial_balance: 10n
+        }, { maxFee: deployFee.amount });
+        const balanceBefore = await contract.call("get_balance");
+        expect(balanceBefore.response).to.deep.equal(10n);
 
-        const args = { amount1: 10, amount2: 20 };
+        const args = { amount1: 10n, amount2: 20n };
         const fee = await account.estimateFee(contract, "increase_balance", args);
         console.log("Estimated invoke fee:", fee);
 
         await account.invoke(contract, "increase_balance", args, { maxFee: fee.amount * 2n });
         console.log("Increased balance");
 
-        const { res: balanceAfter } = await contract.call("get_balance");
-        expect(balanceAfter).to.deep.equal(30n);
+        const balanceAfter = await contract.call("get_balance");
+        console.log("balance after", balanceAfter.response)
+        expect(balanceAfter.response).to.deep.equal(40n);
     });
 });
