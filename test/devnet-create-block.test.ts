8d02a690f9f40afe9033bdbf699e82de2789c5a9
Nathan-SL <nl@shardlabs.io>
2023-04-05 21:04:24 +0300

    Avoid block_hash equality assertion

--- test/devnet-create-block.test.ts
+++ test/devnet-create-block.test.ts
@@ -10,12 +10,10 @@ describe("Devnet create block", function () {
         const latestBlock = await starknet.getBlock();
 
         const emptyBlock1 = await starknet.devnet.createBlock();
-        expect(Number(emptyBlock1.block_hash)).to.deep.equal(Number(latestBlock.block_hash) + 1);
         const emptyBlock1Response = await starknet.getBlock({ blockHash: emptyBlock1.block_hash });
         expect(emptyBlock1Response.transactions).to.be.empty;
 
         const emptyBlock2 = await starknet.devnet.createBlock();
-        expect(Number(emptyBlock2.block_hash)).to.deep.equal(Number(emptyBlock1.block_hash) + 1);
         const emptyBlock2Response = await starknet.getBlock({ blockHash: emptyBlock2.block_hash });
         expect(emptyBlock2Response.transactions).to.be.empty;
     });
