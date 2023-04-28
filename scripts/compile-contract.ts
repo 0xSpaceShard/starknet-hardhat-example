4ee4e3e9a9ec3987ef25603be58c8eaa9d4d1133
Nathan <nl@shardlabs.io>
2023-04-28 17:29:00 +0300

    Adapt to starknet 0.11.0.2 (#106)

--- scripts/compile-contract.ts
+++ scripts/compile-contract.ts
@@ -1,8 +1,12 @@
 import hardhat from "hardhat";
 
 async function main() {
-    await hardhat.run("starknet-compile", {
+    await hardhat.run("starknet-compile-deprecated", {
         paths: ["contracts/contract.cairo"]
+    });
+
+    await hardhat.run("starknet-compile", {
+        paths: ["cairo1-contracts/contract1.cairo"]
     });
 }
 
