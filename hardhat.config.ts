53cd786f6704520bf8e68c39766a704863a2f253
FabijanC <fabijan.corak@gmail.com>
2023-05-03 17:15:38 +0200

    Use custom cairo 1 compilation binaries (#107)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -17,7 +17,9 @@ const config: HardhatUserConfig = {
         // uses the currently active Python environment (hopefully with available Starknet commands!)
         // venv: "active",
         recompile: false,
-        // manifestPath: "path/to/Cargo.toml",
+
+        // the directory containing Cairo 1 compiler binaries
+        // cairo1BinDir: "path/to/to/compiler/target/release"
         network: "integrated-devnet",
         wallets: {
             OpenZeppelin: {
