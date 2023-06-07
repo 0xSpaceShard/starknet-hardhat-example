e84aab1d5bfaccc4920299f1487cdc6657c1537c
Nathan <nl@shardlabs.io>
2023-06-07 10:45:36 +0300

    Add utility func to get n pre-deployed accounts (#112)

--- test/get-predeployed-accounts.test.ts
+++ test/get-predeployed-accounts.test.ts
@@ -1,6 +1,7 @@
 import { expect } from "chai";
 import { starknet } from "hardhat";
 import { TIMEOUT } from "./constants";
+import { getPredeployedOZAccounts } from "./util";
 
 describe("Devnet restart", function () {
     this.timeout(TIMEOUT);
@@ -12,5 +13,18 @@ describe("Devnet restart", function () {
         expect(response[0].initial_balance).to.be.a("number");
         expect(response[0].private_key).to.be.a("string");
         expect(response[0].public_key).to.be.a("string");
+    });
+
+    it("should fetch n number of OZ account classes from predeployed accounts", async () => {
+        const count1 = 4;
+        const accounts1 = await getPredeployedOZAccounts(count1);
+        expect(accounts1).to.be.an("array");
+        expect(accounts1).to.have.lengthOf(count1);
+
+        // Test the ensurePort function with multiple async calls to getAccountFromAddress
+        const count2 = 3;
+        const accounts2 = await getPredeployedOZAccounts(count2);
+        expect(accounts2).to.be.an("array");
+        expect(accounts2).to.have.lengthOf(count2);
     });
 });
