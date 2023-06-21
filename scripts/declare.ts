2acdcebc4dc2b6b44f19e739eef5b339fc596e55
FabijanC <fabijan.corak@gmail.com>
2023-06-21 13:24:04 +0200

    Add configurable declaration script

---
+++ scripts/declare.ts
@@ -0,0 +1,29 @@
+import { starknet } from "hardhat";
+import { ensureEnvVar, getOZAccount } from "../test/util";
+
+/**
+ * Should work if run with:
+ * $ DECLARABLE_CONTRACT=<YOUR_CONTRACT_NAME> npx hardhat run <NAME_OF_THIS_SCRIPT>
+ */
+async function main() {
+    const account = await getOZAccount();
+
+    const contractName = ensureEnvVar("DECLARABLE_CONTRACT");
+    console.log("Declaring", contractName);
+
+    const contractFactory = await starknet.getContractFactory(contractName);
+
+    const declarationTxHash = await account.declare(contractFactory, {
+        maxFee: 1e18
+    });
+
+    const classHash = await contractFactory.getClassHash();
+    console.log(`Declared class ${classHash} in tx ${declarationTxHash}`);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
