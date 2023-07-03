a29623f5657152c1fb0674e2f5cc685e7601b3a0
FabijanC <fabijan.corak@gmail.com>
2023-07-03 16:04:03 +0200

    Fix replacement

--- scripts/replace.ts
+++ scripts/replace.ts
@@ -3,10 +3,22 @@ import { getOZAccount } from "../test/util";
 
 async function main() {
     const account = await getOZAccount();
+
+    // declare and deploy replacable
     const replaceableFactory = await starknet.getContractFactory("replaceable");
     await account.declare(replaceableFactory);
     const replaceableContract = await account.deploy(replaceableFactory);
-    console.log(await replaceableContract.call("foo"));
+
+    // declare replaced
+    const replacedFactory = await starknet.getContractFactory("replaced");
+    await account.declare(replacedFactory);
+
+    // replace
+    console.log("Before replacement:", await replaceableContract.call("foo"));
+    await account.invoke(replaceableContract, "replace", {
+        new_class_hash: await replacedFactory.getClassHash()
+    });
+    console.log("After replacement:", await replaceableContract.call("foo"));
 }
 
 main()
